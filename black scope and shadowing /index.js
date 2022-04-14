e5cd4c14c206dc21377a1ca3478d0caa46203e2a
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-14 18:14:33 +0500

    block scope and shadowing

---
+++ black scope and shadowing /index.js
@@ -0,0 +1,18 @@
+// var a = 100; {
+//     // compound statement
+//     // combine multiple javascript statement in one group 
+//     var a = 33;
+//     let b = 44;
+//     const c = 55;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+const c = 200;
+
+function x() {
+
+}
