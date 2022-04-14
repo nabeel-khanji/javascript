f7511f7d1c46431fddfee0a91afc81dd14280ea9
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-14 17:32:33 +0500

    temporal deadzone

---
+++ temporal dead zone/index.js
@@ -0,0 +1,20 @@
+let a = 3;
+// let a = 7;
+console.log(a);
+const c = 4;
+// c = 3;
+// var a = 3;  
+var b = 30;
+
+
+// type error
+// assigned a value to a constant variable
+
+// SyntaxError
+// a has alrady been declear
+
+// ReferenceError
+// when javascript try to find a specific variable which is not present in a memory
+// var is not defined
+
+// the best way to avoid temoral deadzone is to push all the initialization and declear at the top/
