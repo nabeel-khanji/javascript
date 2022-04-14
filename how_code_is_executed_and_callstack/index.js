c0a88b5897f250988410d3e813268589c222034f
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-14 10:52:03 +0500

    complete how code is executed in callstack

--- how_code_is_executed_and_callstack/index.js
+++ how_code_is_executed_and_callstack/index.js
@@ -4,7 +4,7 @@ function square(num) {
     var ans = num * num;
     return ans;
 }
-var square2 = square(2);
+var square2 = square(n);
 var square4 = square(4);
 console.log(square2);
 console.log(square4);
