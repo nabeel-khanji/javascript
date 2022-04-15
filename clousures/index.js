c7fab228090abfa9dec64b69cb272601f700dc66
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-04-15 11:49:51 +0500

    Closures

---
+++ clousures/index.js
@@ -0,0 +1,40 @@
+// function x() {
+//     var a = 7;
+
+//     function z() {
+//         var a = 9;
+
+//         function y() {
+//             console.log(a);
+//         }
+//         y();
+//     }
+//     z();
+// }
+// x();
+
+var a = 4;
+var b = 555;
+
+function x() {
+    var a = 7;
+    var b = 900
+
+    function z() {
+        var a = 9;
+        // var b =
+        //     800;
+
+        function y() {
+            console.log(a, b);
+        }
+        var a = 100;
+        return y;
+    }
+    var z = z();
+    console.log(z);
+    return z;
+}
+var c = x();
+console.log(c);
+c();
